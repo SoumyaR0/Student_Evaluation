610907d39c5f8ddc0c24f78c174c3faa57684e59
SoumyaR0 <soumyaranjanpatra730@gmail.com>
2025-01-19 09:47:33 +0530

    adding name search to the account/user fetch

--- routes/teacher.js
+++ routes/teacher.js
@@ -24,5 +24,19 @@ router.get('/', async(req,res)=>{
         res.status(500).json({err:'internal server error'});
     }
 });
+router.get('/:Name', async(req,res)=>{
+    try{
+        const Name = req.params.Name;
+        const response = await teacher.find({name:Name});
+        if(!response){
+            return res. status(404).json({error:"person not found"});
+        }
+        console.log('data fetched successfully');
+        res.status(200).json(response);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({err:'internal server error'});
+    }
+});
 
 module.exports = router;
